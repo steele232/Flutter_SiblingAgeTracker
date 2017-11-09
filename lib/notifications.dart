2c8691ba9b882cdf44d2ecc6ee79bffb7a41e58a
Jonathan Steele <jonathan.steele232@gmail.com>
2017-11-09 15:51:07 -0700

    Got the list of switches to come out and work. It took some widget tree debugging. #expanded #material #flexible #constraints

--- lib/notifications.dart
+++ lib/notifications.dart
@@ -41,11 +41,11 @@ class NotificationsPageState extends State<NotificationsPage> {
             textDirection: TextDirection.ltr,
         ),
       ),
-      child: new Column(
-        children: <Widget>[
-          new Material(
-            type: MaterialType.transparency,
-            child: new ListTile(
+      child: new Material(
+        type: MaterialType.transparency,
+        child: new Column(
+          children: <Widget>[
+            new ListTile(
               subtitle: new Text('Birthday Notifications'), //TODO IMPROVE THIS TYPOGRAPHY / SIZE / DARKNESS.
               trailing: new CupertinoSwitch(value: _birthdayIsOn, onChanged: (bool value) {
                 setState(() {
@@ -53,52 +53,41 @@ class NotificationsPageState extends State<NotificationsPage> {
                 });
               }),
             ),
-          ),
-          new Divider(height: 20.0),
-          new Material(
-            type: MaterialType.transparency,
-            child: new Column(
-              children: <Widget>[
-                new ListTile(
-                  subtitle: new Text('Day Of'),
-                  trailing: new CupertinoSwitch(value: _dayOf, onChanged: (bool value) {
-                    setState(() {
-                      _dayOf = value;
-                    });
-                  }),
-                ),
-                new ListTile(
-                  subtitle: new Text('Day Before'),
-                  trailing: new CupertinoSwitch(value: _dayBefore1, onChanged: (bool value) {
-                    setState(() {
-                      _dayBefore1 = value;
-                    });
-                  }),
-                ),
-                new ListTile(
-                  subtitle: new Text('2 Days Before'),
-                  trailing: new CupertinoSwitch(value: _dayBefore2, onChanged: (bool value) {
-                    setState(() {
-                      _dayBefore2 = value;
-                    });
-                  }),
-                ),
+            new Divider(height: 20.0),
+            new Expanded(
+  //            type: MaterialType.transparency,
+              child: new Column(
+                children: <Widget>[
+                  new ListTile(
+                    subtitle: new Text('Day Of'),
+                    trailing: new CupertinoSwitch(value: _dayOf, onChanged: (bool value) {
+                      setState(() {
+                        _dayOf = value;
+                      });
+                    }),
+                  ),
+                  new ListTile(
+                    subtitle: new Text('Day Before'),
+                    trailing: new CupertinoSwitch(value: _dayBefore1, onChanged: (bool value) {
+                      setState(() {
+                        _dayBefore1 = value;
+                      });
+                    }),
+                  ),
+                  new ListTile(
+                    subtitle: new Text('2 Days Before'),
+                    trailing: new CupertinoSwitch(value: _dayBefore2, onChanged: (bool value) {
+                      setState(() {
+                        _dayBefore2 = value;
+                      });
+                    }),
+                  ),
 
-              ],
-            )
-          ),
-          new Center(
-            child: new Text(
-              'Test Text',
-              textAlign: TextAlign.center,
-              textScaleFactor: 1.0,
-              textDirection: TextDirection.ltr,
-              style: Theme.of(context).textTheme.subhead,
-              overflow: TextOverflow.clip,
-              softWrap: true,
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
